4be2cb6d7db129cef731543e1e63163e5a311d7d
Killerpommes <killerpommes88@web.de>
2013-03-24 17:18:55 +0100

    updated korean translation

--- lang/KR_Lang.lua
+++ lang/KR_Lang.lua
@@ -1,20 +1,20 @@
 RCText = {
 	PrintOut = {
-		Say = "loaded! /rc to open RuneCraft",
-		Description = "Its a combination Guid for Runes."
+		Say = "로드되었습니다.",
+		Description = "룬 조합 가이드입니다."
 	},
 	Menu = {
-		Grade = "Grade ",
-		Tier1 = "Rank ",
-		Tier2 = " Runes",
-		Buildable = "Buildable Runes",
-		Needed = "Needed Runes"
+		Grade = "메뉴 ",
+		Tier1 = "랭크 ",
+		Tier2 = " 룬",
+		Buildable = "제작가능 룬",
+		Needed = "필요한 룬"
 	},
 	Runes = {
 		Empty = "",	-- comb 5 = 1
-		Deactive = "|cffF5F5F5Deactivated|r", -- comb 5 = 2
-		NoMake = "|cffFF4500Not produceable|r", --comb 5 = 3
-		TreasureHunt = "|cff32CD32From mini-games|r", --comb 5 = 4
-		NoMakeTreasureHunt = "|cffFF4500Not produceable|r\n\n|cff32CD32From mini-games|r" --comb 5 = 5
+		Deactive = "|cffF5F5F5비활성화 됨|r", -- comb 5 = 2
+		NoMake = "|cffFF4500생산 된 할 수 없습니다|r", --comb 5 = 3
+		TreasureHunt = "|cff32CD32미니 가지 게임에서|r", --comb 5 = 4
+		NoMakeTreasureHunt = "|cffFF4500생산 된 할 수 없습니다|r\n\n|cff32CD32미니 가지 게임에서|r" --comb 5 = 5
 	}
 }
