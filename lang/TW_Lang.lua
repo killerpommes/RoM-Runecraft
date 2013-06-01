1dd0c133c59f4e64d3c1176e2e09bfcf8875cdcc
Killerpommes <killerpommes88@web.de>
2013-06-01 08:32:22 +0200

    add TW UI Translation from e65130; changelog update

--- lang/TW_Lang.lua
+++ lang/TW_Lang.lua
@@ -1,20 +1,20 @@
 RCText = {
 	PrintOut = {
-		Say = "loaded! /rc to open RuneCraft",
-		Description = "Its a combination Guid for Runes."
+		Say = "請輸入 /rc 打開工具",
+		Description = "這是一個符文組合公式工具."
 	},
 	Menu = {
-		Grade = "Grade ",
-		Tier1 = "Rank ",
-		Tier2 = " Runes",
-		Buildable = "Buildable Runes",
-		Needed = "Needed Runes"
+		Grade = "等級 ",
+		Tier1 = "階層 ",
+		Tier2 = " 符文",
+		Buildable = "符文組合",
+		Needed = "所需要的符文"
 	},
 	Runes = {
 		Empty = "",	-- comb 5 = 1
-		Deactive = "|cffF5F5F5Deactivated|r", -- comb 5 = 2
-		NoMake = "|cffFF4500Not produceable|r", --comb 5 = 3
-		TreasureHunt = "|cff32CD32From mini-games|r", --comb 5 = 4
-		NoMakeTreasureHunt = "|cffFF4500Not produceable|r\n\n|cff32CD32From mini-games|r" --comb 5 = 5
+		Deactive = "|cffF5F5F5未開放合成|r", -- comb 5 = 2
+		NoMake = "|cffFF4500未開放|r", --comb 5 = 3
+		TreasureHunt = "|cff32CD32提取遊戲資料|r", --comb 5 = 4
+		NoMakeTreasureHunt = "|cffFF4500未開放|r\n\n|cff32CD32提取遊戲資料|r" --comb 5 = 5
 	}
 }
